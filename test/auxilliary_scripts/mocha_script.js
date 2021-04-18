b0e5791fc5d63e5fbd9e190c1a4e7e65fceffa17
= <=>
2021-04-18 14:25:33 +0200

    added a logo

--- test/auxilliary_scripts/mocha_script.js
+++ test/auxilliary_scripts/mocha_script.js
@@ -5,13 +5,8 @@ describe("power_and_hello_world", function() {
 
     it("raises to n-th power", function() {
       assert.equal(pow(2, 3), 8);
-      // assert.equal(pow(3, 5), 243);
     });
     it("should return hello world in different langauges", function() {
       assert.equal(helloworld("spanish"), "Hola Mundo");
-      // assert.equal(helloworld("deutsch"), "Hallo Welt");
     });
-  
   });
-// console.log(pow(4,2))
-// console.log(helloworld("spanish"))
